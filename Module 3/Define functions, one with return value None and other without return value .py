3987a81201e9a54cdb1335a7a24469bfc6ea034e
João Bastos Garrido <151746625+UtopiaJBG@users.noreply.github.com>
2025-04-14 00:26:28 +0000

    a a a

---
+++ Module 3/Define functions, one with return value None and other without return value .py
@@ -0,0 +1,10 @@
+# Define functions, one with return value None and other without return value
+def MJ():
+    print('Michael Jackson')
+    
+def MJ1():
+    print('Michael Jackson')
+    return(None)
+
+print(MJ())
+print(MJ1())
